6272a30c9c678fe72a0602ddbe2e51c3fc7caa1e
Eranda Madusanka <madushankaeranda56@gmail.com>
2025-01-27 23:44:25 +0530

    homepage create

---
+++ src/components/TopicBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './topicbox.css'
+
+
+export default function TopicBox() {
+  return (
+    <div>
+        <div className="topicBox">
+            <span>This is the topic</span>
+        </div>
+    </div>
+  )
+}
